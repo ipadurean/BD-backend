b42a90ba733e5ada5b08967a2aeafdafebc6cfcd
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-10-21 13:38:14 -0500

    prepared for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post '/auth', to: 'authentication#login'
   get '/current_user', to: 'authentication#profile'
   get '/*a', to: 'application#not_found'
+  root 'welcome#index'
    
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
